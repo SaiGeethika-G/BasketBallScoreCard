d5b867f82cc4771c22d6838f2f32dec410db00f3
Sai Geethika Gudapati <79441278+SaiGeethika-G@users.noreply.github.com>
2022-07-15 09:33:34 +0530

    Update index.js
    Added functions for buttons

---
+++ index.js
@@ -0,0 +1,20 @@
+let score1=document.querySelector(".homePt").textContent;
+let score2=document.querySelector(".guestPt").textContent;
+function addOneHome(){
+    score1=parseInt(score1)+1;
+}
+function addTwoHome(){
+    score1=parseInt(score1)+2;
+}
+function addThreeHome(){
+    score1=parseInt(score1)+3;
+}
+function addOneGuest(){
+    score2=parseInt(score2)+1;
+}
+function addTwoGuest(){
+    score2=parseInt(score2)+2;
+}
+function addThreeGuest(){
+    score2=parseInt(score2)+3;
+}
